36b2dd5d5fab5877e75c289b61d423aea7064dad
mohamed hassan <46112800+mohamed9623@users.noreply.github.com>
2022-01-09 22:10:40 +0200

    Update profilePage.php

--- task5/profilePage.php
+++ task5/profilePage.php
@@ -1,1 +1,17 @@
 
+<?php
+
+
+echo "<h1> Blog </h1>";
+
+echo "<P>TO DELETE FULL BLOG PRESS ON BUCKET IMG  &#8594 &#8594 </P><a href='delete.php'><img src='css/Delete-Bin-Trash-PNG-Free-Download.png'  width='50px'  height='50px'></a><br>";
+
+$file = fopen('text.txt',"r") or die('unable to open file');
+while(!feof($file)) {
+    $hassan = fgets($file);
+    echo $hassan."<br>";
+}
+
+
+fclose($file);
+?>
