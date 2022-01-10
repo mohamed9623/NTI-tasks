0dd42139477a9b291282f1358735d102b83853ee
mohamed hassan <46112800+mohamed9623@users.noreply.github.com>
2022-01-10 09:20:15 +0200

    Update profilePage.php

--- task5/profilePage.php
+++ task5/profilePage.php
@@ -12,7 +12,6 @@ while(!feof($file)) {
     $hassan = fgets($file);
     echo $hassan."<br>";  
     if (!empty($hassan)) {
-        //        echo $l . " <a href='test1.php'>Delete Article</a><br>";
                 setcookie('row', $hassan, time() + 86400, '/');
                 echo "<P>TO DELETE ONE LINE PRESS ON BUCKET IMG  &#8594 &#8594 </P><a href='delete1.php'><img src='css/PikPng.com_trash-png_471206.png'  width='50px'  height='50px'></a><br>";
             }}
